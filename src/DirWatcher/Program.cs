973deb3be7b78a15d5b93a3e0ba725229b4c8c47
Martin <prixedox@gmail.com>
2026-06-27 19:46:55 +0200

    wire up di

--- src/DirWatcher/Program.cs
+++ src/DirWatcher/Program.cs
@@ -1,23 +1,24 @@
+using DirWatcher.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var snapshotDir = Path.Combine(builder.Environment.ContentRootPath, "App_Data", "snapshots");
+builder.Services.AddSingleton<ISnapshotStore>(_ => new JsonSnapshotStore(snapshotDir));
+builder.Services.AddSingleton<ChangeDetectionService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapStaticAssets();
 
 app.MapControllerRoute(
